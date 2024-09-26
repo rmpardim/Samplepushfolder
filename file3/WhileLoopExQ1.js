c84a53b743a8dea06780bfb9263496545cfdf0f4
Rosangela Pardim <rmpardim@gmail.com>
2024-09-26 20:31:21 +1000

    First commit

---
+++ file3/WhileLoopExQ1.js
@@ -0,0 +1,9 @@
+function CalculateYearsDep(Deposit) {
+  let deposit = parseFloat(Deposit);
+
+
+  
+  result = studName + ", " + college + ", " + courseId;
+  document.getElementById("message").innerHTML = result;
+  return false;
+}
